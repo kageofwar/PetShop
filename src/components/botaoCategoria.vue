7d00f1f59c356ae8d6b4b1bdf597717f26af6bfd
Vinicius Casotti de Campos Oliveira <viniciusccoliveira@PREFEITURA.SP.GOV.BR>
2024-02-09 17:33:34 -0300

    first

---
+++ src/components/botaoCategoria.vue
@@ -0,0 +1,24 @@
+<template>
+    <a href="#" class="sombra flex justify-center items-center text-[20px] h-[50px] w-[250px] overflow-hidden rounded-[33px] transition ease-in-out delay-0 hover:-translate-y-1 hover:scale-110 hover:bg-[#FFFF] duration-150">
+        
+        <img :src="`src/assets/icons/categoriaIcons/${imageName}`" alt="imagem da categoria">
+        <h2 class="">{{categoriaNome}}</h2>
+    </a>
+</template>
+
+<script setup> 
+
+defineProps([
+    'categoriaNome',
+    'imageName'
+]);
+
+</script>
+
+<style>
+
+.sombra {
+    box-shadow: 1px 2px 5px 0px rgba(0, 0, 0, 0.2);
+}
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="h-screen bg-[#5096B5]">
+    <div>
+      <div class="flex justify-center items-center text-white h-[41px] bg-[#275BA8]">
+        <img src="../assets/icons/Magnetic Card.png" alt="credit card" class="h-[30px] mb-[8px]">
+        <p class="p-[10px]">Parcele em até 3x sem juros. Com parecelas a partir de $75.</p>
+        <img src="../assets/icons/Magnetic Card.png" alt="credit card" class="h-[30px] mb-[8px]">
+      </div>
+      <div class="flex justify-center space-x-[70px] items-center h-[100px] bg-white">
+        <img src="../assets/PetFuad-removebg.png" alt="logo da loja" class="h-[100px]">
+        <div class="flex justify-end w-[60%]">
+          <input type="search" name="" id="" placeholder="O que seu Pet precisa?"
+            class="text-center bg-[#275BA8] h-[66px] w-[100%] border rounded-[200px] placeholder:text-white">
+          <a href="#" class="absolute mr-5"><img src="../assets/icons/Search.png" alt="imagem de lupa"></a>
+        </div>
+        <div class="flex space-x-6">
+          <img src="../assets/icons/Shop.png" alt="shop">
+          <img src="../assets/icons/Shopping Cart.png" alt="carrinho">
+          <img src="../assets/icons/Phone.png" alt="telefone">
+          <img src="../assets/icons/Name.png" alt="perfil">
+        </div>
+      </div>
+    </div>
+
+    <div class="bg-white space-x-11 flex justify-center items-center h-[150px] mt-[20px]">
+      <botaoCategoria imageName="DogBone.png" categoriaNome="Cachorros" />
+      <botaoCategoria imageName="PawPrints.png" categoriaNome="Gatos" />
+      <botaoCategoria imageName="Feather.png" categoriaNome="Pássaros" />
+      <botaoCategoria imageName="Fish.png" categoriaNome="Outros Pets" />
+      <botaoCategoria imageName="SupplementBottle.png" categoriaNome="Remédios" />
+      <botaoCategoria imageName="DogHouse.png" categoriaNome="Casa" />
+    </div>
+    
+
+  </div>
+</template>
+
+<script setup>
+import botaoCategoria from '@/components/botaoCategoria.vue';
+
+</script>
