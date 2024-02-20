39befd77ed167f8cf7f8fe4bd24d49a441869504
Vinicius Casotti de Campos Oliveira <viniciusccoliveira@PREFEITURA.SP.GOV.BR>
2024-02-20 18:00:51 -0300

    haha tlgd

---
+++ src/components/botaoRecomendacao.vue
@@ -0,0 +1,19 @@
+<template>
+    <a href="" class="border rounded-t-[50px] rounded-bl-[50px] rounded p-3 text-white w-[90%] h-[100%]">
+    <div>
+        <div class="ml-3">
+            <img :src="`src/assets/icons/recomendacaoIcons/${nomeDaImagem}`" alt="icone">
+            <p class="text-[2vh]">{{ RecomendacaoTitulo }}</p>
+        </div>
+    </div>
+    </a>
+</template>
+
+<script setup>
+
+defineProps([
+    'nomeDaImagem',
+    'RecomendacaoTitulo'
+]);
+
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,9 +2,9 @@
   <div class="h-full bg-[#5096B5]">
     <div>
       <div class="flex justify-center items-center text-white h-[41px] bg-[#275BA8]">
-        <img src="../assets/icons/Magnetic Card.png" alt="credit card" class="h-[30px] mb-[8px]">
+        <img src="../assets/icons/headerIcons/Magnetic Card.png" alt="credit card" class="h-[30px] mb-[8px]">
         <p class="p-[10px]">Parcele em até 3x sem juros. Com parecelas a partir de $75.</p>
-        <img src="../assets/icons/Magnetic Card.png" alt="credit card" class="h-[30px] mb-[8px]">
+        <img src="../assets/icons/headerIcons/Magnetic Card.png" alt="credit card" class="h-[30px] mb-[8px]">
       </div>
 
       <div class="flex justify-center space-x-[70px] items-center h-[70px] bg-white">
@@ -12,13 +12,13 @@
         <div class="flex justify-end w-[50%]">
           <input type="search" name="" id="" placeholder="O que seu Pet precisa?"
             class="text-center bg-[#275BA8] h-[55px] w-[100%] border rounded-[200px] placeholder:text-white">
-          <a href="#" class="absolute mr-5"><img src="../assets/icons/Search.png" alt="imagem de lupa"></a>
+          <a href="#" class="absolute mr-5"><img src="../assets/icons/headerIcons/Search.png" alt="imagem de lupa"></a>
         </div>
         <div class="flex space-x-6 h-9">
-          <img src="../assets/icons/Shop.png" alt="shop">
-          <img src="../assets/icons/Shopping Cart.png" alt="carrinho">
-          <img src="../assets/icons/Phone.png" alt="telefone">
-          <img src="../assets/icons/Name.png" alt="perfil">
+          <img src="../assets/icons/headerIcons/Shop.png" alt="shop">
+          <img src="../assets/icons/headerIcons/Shopping Cart.png" alt="carrinho">
+          <img src="../assets/icons/headerIcons/Phone.png" alt="telefone">
+          <img src="../assets/icons/headerIcons/Name.png" alt="perfil">
         </div>
       </div>
     </div>
@@ -68,31 +68,51 @@
           <h2 class="text-[5vh] text-[#5096B5]">Seu Pet também pode gostar ❤️</h2>
         </div>
 
-        <div class="mt-20">
+        <!--<div class="mt-20">
           <h2 class="text-[3vh] text-[#5096B5] ml-28 mb-5">Cachorros</h2>
           <div class="grid grid-cols-4 justify-items-center">
-            <CardRecomendacao nomeProduto="Ração Vital Pro Pedigree 1kg" valor="99,00" imagemProduto="pedrigre.png"/>
-            <CardRecomendacao nomeProduto="Ração Vital Pro Pedigree 1kg" valor="99,00" imagemProduto="pedrigre.png"/>
-            <CardRecomendacao nomeProduto="Ração Vital Pro Pedigree 1kg" valor="99,00" imagemProduto="pedrigre.png"/>
-            <CardRecomendacao nomeProduto="Ração Vital Pro Pedigree 1kg" valor="99,00" imagemProduto="pedrigre.png"/>
+            <CardRecomendacao nomeProduto="Ração Vital Pro Pedigree 1kg" valor="99,00" imagemProduto="pedrigre.png" />
+            <CardRecomendacao nomeProduto="Ração Vital Pro Pedigree 1kg" valor="99,00" imagemProduto="pedrigre.png" />
+            <CardRecomendacao nomeProduto="Ração Vital Pro Pedigree 1kg" valor="99,00" imagemProduto="pedrigre.png" />
+            <CardRecomendacao nomeProduto="Ração Vital Pro Pedigree 1kg" valor="99,00" imagemProduto="pedrigre.png" />
           </div>
         </div>
 
         <div class="mt-20">
           <h2 class="text-[3vh] text-[#5096B5] ml-28 mb-5">Gatos</h2>
           <div class="grid grid-cols-4 justify-items-center">
-            <CardRecomendacao nomeProduto="Ração Whiskas Carne 10kg" valor="99,00" imagemProduto="whiskas.webp"/>
-            <CardRecomendacao nomeProduto="Ração Whiskas Carne 10kg" valor="99,00" imagemProduto="whiskas.webp"/>
-            <CardRecomendacao nomeProduto="Ração Whiskas Carne 10kg" valor="99,00" imagemProduto="whiskas.webp"/>
-            <CardRecomendacao nomeProduto="Ração Whiskas Carne 10kg" valor="99,00" imagemProduto="whiskas.webp"/>
+            <CardRecomendacao nomeProduto="Ração Whiskas Carne 10kg" valor="99,00" imagemProduto="whiskas.webp" />
+            <CardRecomendacao nomeProduto="Ração Whiskas Carne 10kg" valor="99,00" imagemProduto="whiskas.webp" />
+            <CardRecomendacao nomeProduto="Ração Whiskas Carne 10kg" valor="99,00" imagemProduto="whiskas.webp" />
+            <CardRecomendacao nomeProduto="Ração Whiskas Carne 10kg" valor="99,00" imagemProduto="whiskas.webp" />
           </div>
-        </div>
+        </div>-->
       </div>
-      <img src="../assets/Vector (1).png" alt="">
+      <img src="../assets/Vector (1).png" alt="" class="-translate-y-[1px]">
 
     </div>
 
-
+    <div class="flex justify-center">
+      <div class="w-[80%]">
+        <h2 class="text-white text-[5vh] m-5">Pet Shop Online Pet Fuad</h2>
+        <div class="border rounded-t-[50px] rounded-bl-[50px] rounded p-5 text-white">
+          <div class="flex">
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius numquam delectus, architecto quis fugit
+              itaque, possimus distinctio exercitationem error a magni. Fugit mollitia officia ipsum nesciunt quas
+              consequatur temporibus doloribus!</p>
+            <img src="../assets/Pets.png" alt="" class="h-[12vh]">
+          </div>
+          <div class="grid grid-cols-6 justify-items-center items-center">
+            <botaoRecomendacao nomeDaImagem="Timetable.png" RecomendacaoTitulo="Compra Programada"/>
+            <botaoRecomendacao nomeDaImagem="Discount.png" RecomendacaoTitulo="Ofertas Exclusivas"/>
+            <botaoRecomendacao nomeDaImagem="Shop.png" RecomendacaoTitulo="Conheça nossas lojas"/>
+            <botaoRecomendacao nomeDaImagem="Barbershop.png" RecomendacaoTitulo="Serviço de tosa"/>
+            <botaoRecomendacao nomeDaImagem="WhatsApp.png" RecomendacaoTitulo="WhatsApp"/>
+            <botaoRecomendacao nomeDaImagem="Instagram.png" RecomendacaoTitulo="Instagram"/>
+          </div>
+        </div>
+      </div>
+    </div>
 
   </div>
 </template>
@@ -100,6 +120,7 @@
 <script setup>
 import botaoCategoria from '@/components/botaoCategoria.vue';
 import CardRecomendacao from '@/components/CardRecomendacao.vue'
+import botaoRecomendacao from '@/components/botaoRecomendacao.vue'
 
 import { ref, onMounted, onBeforeUnmount } from 'vue';
 
