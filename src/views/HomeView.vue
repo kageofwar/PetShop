e66ed56ab5588ca0d466645d489ad4e46a1f489b
Vinicius Casotti de Campos Oliveira <viniciusccoliveira@PREFEITURA.SP.GOV.BR>
2024-03-04 17:58:57 -0300

    xd

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="h-full bg-[--azul-claro]">
+
     <div>
       <div class="flex justify-center items-center text-white h-[41px] bg-[--azul-escuro]">
         <img src="../assets/icons/headerIcons/Magnetic Card.png" alt="credit card" class="h-[30px] mb-[8px]">
@@ -7,7 +8,7 @@
         <img src="../assets/icons/headerIcons/Magnetic Card.png" alt="credit card" class="h-[30px] mb-[8px]">
       </div>
 
-      <div class="flex justify-center space-x-[70px] items-center h-[70px] bg-white">
+      <div class="header_pesquisar flex justify-center space-x-[70px] items-center h-[70px] bg-white">
         <img src="../assets/PetFuad-removebg.png" alt="logo da loja" class="h-[100px]">
         <div class="flex justify-end w-[50%]">
           <input type="search" name="" id="" placeholder="O que seu Pet precisa?"
@@ -39,8 +40,7 @@
       </div>
     </div>
 
-
-    <div class="bg-white space-x-11 flex justify-center items-center h-[100px] mt-[20px]">
+    <div class="header_categorias bg-white space-x-11 flex justify-center items-center h-[100px]">
       <botaoCategoria imageName="DogBone.png" categoriaNome="Cachorros" />
       <botaoCategoria imageName="PawPrints.png" categoriaNome="Gatos" />
       <botaoCategoria imageName="Feather.png" categoriaNome="Pássaros" />
@@ -108,8 +108,29 @@
         </div>
 
       </div>
-      <img src="../assets/Vector (1).png" alt="" class="-translate-y-[1px]">
+      <img src="../assets/Vector (1).png" alt="" class="-translate-y-[50px]">
+    </div>
 
+    <div class="m-[10vh_0]">
+      <div>
+        <h2 class="text-white ml-20 text-[4vh]">Algumas das marcas que trabalhamos</h2>
+      </div>
+      <div class="flex justify-center items-center">
+        <div class="grid grid-cols-6 w-[90%] gap-1">
+          <div class="h-[25vh] bg-white sombra flex justify-center items-center"><img src="../assets/logoMarcas/Purina-Logo.png" alt="purina logo"></div>
+          <div class="h-[25vh] bg-white sombra flex justify-center items-center"><img src="../assets/logoMarcas/premier.png" alt="premier logo"></div>
+          <div class="h-[25vh] bg-white sombra flex justify-center items-center"><img src="../assets/logoMarcas/magnus-2.png" alt="logo magnus"></div>
+          <div class="h-[25vh] bg-white sombra flex justify-center items-center"><img src="../assets/logoMarcas/whiskas-logo.png" alt="logo whiskas"></div>
+          <div class="h-[25vh] bg-white sombra flex justify-center items-center"><img src="../assets/logoMarcas/pedigree-logo-6.png" alt="logo pedigree"></div>
+          <div class="h-[25vh] bg-white sombra flex justify-center items-center"><img src="../assets/logoMarcas/premierpet.jpg" alt="logo premier pet"></div>
+          <div class="h-[25vh] bg-white sombra flex justify-center items-center"><img src="../assets/logoMarcas/logo-origens.webp" alt="logo origens"></div>
+          <div class="h-[25vh] bg-white sombra flex justify-center items-center"><img src="../assets/logoMarcas/special-dog.png" alt="logo special dog" class="h-[90%]"></div>
+          <div class="h-[25vh] bg-white sombra flex justify-center items-center"><img src="../assets/logoMarcas/logo-pipicat.jpg" alt="logo pipicat" class="h-[90%]"></div>
+          <div class="h-[25vh] bg-white sombra flex justify-center items-center"><img src="../assets/logoMarcas/optimun.jfif" alt="logo optimun" class="h-[90%]"></div>
+          <div class="h-[25vh] bg-white sombra flex justify-center items-center"><img src="../assets/logoMarcas/special-cat.jfif" alt="logo special cat" class="h-[90%]"></div>
+          <div class="h-[25vh] bg-white sombra flex justify-center items-center"><img src="../assets/logoMarcas/royal-canin.webp" alt="logo royal canin" class="h-[90%]"></div>
+        </div>
+      </div>
     </div>
 
     <div class="flex justify-center">
@@ -134,11 +155,91 @@
       </div>
     </div>
 
-    <div>
-      
+    <div class="flex space-x-4 bg-white h-20 rounded-t-3xl mt-20 items-center">
+      <div class="w-[40%] flex items-center justify-center ml-10 text-[2.4vh] flex-col">
+        <h2>Cadastre-se e receba nossas promoções e novidades</h2>
+        <h2>antes de todo mundo!</h2>
+      </div>
+      <input type="email" name="" id="" class="bg-[#D9D9D9] w-[28%] h-14 rounded-full pl-4" placeholder="Seu E-mail">
+      <select name="pet" id="" class="bg-[#D9D9D9] w-[15%] h-14 rounded-full pl-4 text-opacity-10">
+        <option disabled selected class="">Qual o seu Pet?</option>
+        <option value="volvo">Nenhum</option>
+        <option value="volvo">1</option>
+        <option value="saab">2</option>
+        <option value="opel">3</option>
+        <option value="audi">4</option>
+      </select>
+      <button class="border-[2px] w-[10%] h-14 rounded-full border-black">Cadastrar</button>
     </div>
-  </div>
-</template>
+    
+    <div class="flex items-center justify-center bg-[#275BA8] text-white space-x-20">
+
+      <div>
+        <h2 class="text-[3vh] font-bold">Sobre Nós</h2>
+        <li class="list-none font-light">
+          <ul>Quem Somos</ul>
+          <ul>Nossa Loja</ul>
+          <ul>Seja um Funcionario</ul>
+          <ul>Quem Somos</ul>
+        </li>
+      </div>
+
+      <div>
+        <h2 class="text-[3vh] font-bold">Dúvidas</h2>
+        <li class="list-none font-light">
+          <ul>Política de Entregas</ul>
+          <ul>Política de privacidade</ul>
+          <ul>Ouvidoria</ul>
+        </li>
+      </div>
+
+      <div>
+        <h2 class="text-[3vh] font-bold">Fale com a gente!</h2>
+        <li class="list-none font-light">
+          <ul>example@email.com</ul>
+          <ul>(11) 99999-9999</ul>
+        </li>
+      </div>
+      
+      <div class="flex border-[2px] p-2 rounded-full border-black">
+        <div class="flex flex-col justify-center items-center">
+          <h2>Compre também</h2>
+          <h2>pelo WhatsApp!</h2>
+        </div>
+        <img src="../assets/icons/footerIcons/WhatsApp.png" alt="">
+      </div>
+
+      <div>
+        <div class="flex flex-col justify-center items-center p-5 gap-2">  
+          <h2>Redes Sociais</h2>
+          <div class="flex space-x-4">
+            <img src="../assets/icons/footerIcons/redesSociais/instagram-icon.svg" alt="">
+            <img src="../assets/icons/footerIcons/redesSociais/facebook.png" alt="">
+            <img src="../assets/icons/footerIcons/redesSociais/linkedin.png" alt="">
+            <img src="../assets/icons/footerIcons/redesSociais/pinterest.png" alt="">
+          </div>
+        </div>
+
+        <div>
+          <div class="flex flex-col justify-center items-center p-5">
+            <h2>Formas Pagamento</h2>
+            <div class="flex justify-center items-center space-x-2">
+              <img src="../assets/icons/footerIcons/formasPagamento/elo-logo.png" class="w-[50px] h-[20px]" alt="">
+              <img src="../assets/icons/footerIcons/formasPagamento/visa-logo.png" class="w-[48px] h-[17px]" alt="">
+              <img src="../assets/icons/footerIcons/formasPagamento/hypercard-logo.png" class="w-[40px] h-[18px]" alt="">
+              <img src="../assets/icons/footerIcons/formasPagamento/barcode-icon.png" class="h-[20px]" alt="">
+              <img src="../assets/icons/footerIcons/formasPagamento/pix-transparent.svg" class="h-[25px]" alt="">
+              <img src="../assets/icons/footerIcons/formasPagamento/mastercard-logo.png" alt="">
+              <img src="../assets/icons/footerIcons/formasPagamento/dinersclub-logo.png" class="w-[30px] h-[20px]" alt="">
+              <img src="../assets/icons/footerIcons/formasPagamento/americanexpress-logo.png" alt="">
+            </div>
+          </div>
+        </div>
+      </div>
+        
+    </div>
+    </div>
+  </template>
 
 <script>
 import botaoCategoria from '@/components/botaoCategoria.vue';
@@ -162,7 +263,23 @@ export default {
     return {
       modules: [Autoplay, Pagination, Navigation],
     };
+  },
+  methods() {
+    function scrollFunction() {
+      if (document.documentElement.scrollTop > 10) {
+          document.getElementById("header_categorias").style.display = "none";
+      } else {
+          document.getElementById("header_categorias").style.display = "block";
+      };
+    }
+  },
+  mounted() {
+    window.addEventListener("scroll", this.scrollFunction);
+  },
+  beforeDestroy() {
+    window.removeEventListener("scroll", this.scrollFunction);
   }
+  
 };
 
 </script>
