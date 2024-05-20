c921337aac3d999ba528adbbd1a8b16171e167a7
Vinicius Casotti de Campos Oliveira <viniciusccoliveira@PREFEITURA.SP.GOV.BR>
2024-05-20 16:06:33 -0300

    é isso ai

--- src/components/Auxiliar/visualizarProdutoAux/produtosSimilares.vue
+++ src/components/Auxiliar/visualizarProdutoAux/produtosSimilares.vue
@@ -1,8 +1,8 @@
 <template>
       <div class="flex flex-col bg-white rounded-2xl">
-        <div class="flex items-center justify-center font-semibold"><h2 class="text-[3vh] mb-2">Produtos Similares</h2><img src="../../../assets/icons/PriceTag.png" alt="price tag" class="h-7"></div>
-        <div class="flex w-[145vh] h-[22vh]">
-          <swiper :slidesPerView="4" :spaceBetween="0" :modules="modules" class="mySwiper" :autoplay="{ delay: 1000, disableOnInteraction: false, }">
+        <div class="flex items-center justify-center"><h2 class="text-[3vh] mb-2">Produtos Recomendados</h2><img src="../../../assets/icons/PriceTag.png" alt="price tag" class="h-7"></div>
+        <div class="flex h-[20vh]">
+          <swiper :slidesPerView="3" :spaceBetween="0" :modules="modules" class="mySwiper" :autoplay="{ delay: 2200, disableOnInteraction: false, }">
           <swiper-slide>
             <div class="flex mt-1 ml-1 w-[20vh] h-[19vh] justify-center items-center rounded-3xl sombra flex-col">
               <img src="../../../assets/produtos/pedrigre.png" alt="" class="h-[15vh] w-[15vh]">
--- src/views/VisualizarProduto.vue
+++ src/views/VisualizarProduto.vue
@@ -5,18 +5,20 @@
     <headerCategoria />
 
     <div class="flex justify-center items-center">
-      <div class="grid grid-cols-2 bg-white items-center mb-1 mt-1 w-[80%]">
+      <div class="grid grid-cols-2 bg-white items-center mb-1 mt-1 w-[80%] rounded-t-xl">
         <div>
-          <div class="grid grid-cols-3 divide-x-2">
-            <div class="w-[100%] flex justify-center items-center">
-              <img src="../assets/logoMarcas/pedigree-logo-6.png" alt="logo da marca" class="h-10">
-            </div>
-            <div class="text-[4vh] w-[100%] flex items-center justify-center">
-              🐶
-            </div>
-            <div class="w-[100%] flex items-center justify-center">
-              <img src="../assets/icons/compartilhar.svg" alt="" class="h-10">
-              <img src="../assets/icons/coracao-favorito.svg" alt="" class="h-10">
+          <div class="pl-28">
+            <div class="grid grid-cols-3 divide-x-2">
+              <div class="w-[100%] flex justify-center items-center">
+                <img src="../assets/logoMarcas/pedigree-logo-6.png" alt="logo da marca" class="h-10">
+              </div>
+              <div class="text-[4vh] w-[100%] flex items-center justify-center">
+                🐶
+              </div>
+              <div class="w-[100%] flex items-center justify-center">
+                <img src="../assets/icons/compartilhar.svg" alt="" class="h-10">
+                <img src="../assets/icons/coracao-favorito.svg" alt="" class="h-10">
+              </div>
             </div>
           </div>
 
@@ -36,17 +38,16 @@
                 </div>
             </div>
 
-          <div class="flex justify-center items-center">
-            <img :src="produto.img_url" alt="imagem do produto" class="h-[50vh]">
+            <div class="flex justify-center items-center">
+              <img :src="produto.img_url" alt="imagem do produto" class="h-[50vh]">
+            </div>
           </div>
         </div>
-      </div>
 
         <div>
-
-          <div class="flex flex-col items-center bg-white rounded-[20px] h-[46vh] p-5">
-            <p class=" w-[500px] text-[3.5vh] flex text-center">{{ produto.titulo }}</p>
-            <div class="flex justify-center text-[2.5vh]">
+          <div class="flex flex-col  bg-white rounded-[20px] h-[46vh] p-5">
+            <p class=" w-[500px] text-[3.5vh] flex">{{ produto.titulo }}</p>
+            <div class="flex text-[2.5vh]">
               <div class="flex space-x-2">
                 <p class="">Avaliação ⭐⭐⭐⭐⭐</p>
                 <h2>||</h2>
@@ -54,42 +55,36 @@
               </div>
             </div>
             <a href="#descricao" class="font-bold text-gray-300"><u>Ver Descrição Completa</u></a>
-            <h2 class="text-[5vh] font-semibold mt-5 ">R${{ produto.valor }}</h2>
-            <h2 class="">À vista no PIX com até 10% OFF</h2>
-            <button
-              class="bg-[--azul-escuro] w-[25vh] h-[7vh] flex justify-center items-center rounded-3xl text-white font-bold text-[3.2vh] mt-[4vh]">Comprar</button>
-          </div>
-
-          <div class="flex items-center mt-1 bg-white rounded-[20px] p-1 h-[13vh]">
-            <div class="ml-5">
-              <div class="flex items-center text-black text-[3vh] space-x-2">
-                <img src="../assets/icons/Truck.png" class="h-[5vh]">
-                <h2>Frete e Prazo: </h2>
-              </div>
-              <div class="flex space-x-8">
-                <input type="text"
-                  class="flex justify-center items-center border-[1px] border-black rounded-full placeholder:text-center"
-                  placeholder="Digite seu CEP">
-                <button
-                  class="bg-[--azul-escuro] w-[20vh] h-[5vh] flex justify-center items-center rounded-3xl text-white text-[3.2vh]">Calcular</button>
-              </div>
+            <div class="grid grid-cols-2 items-center">
+              <h2 class="text-[4vh] font-semibold mt-5 ">R${{ produto.valor }}</h2>
+              <button class="bg-[--azul-escuro] w-[25vh] h-[7vh] flex justify-center items-center rounded-3xl text-white font-bold text-[3.2vh] mt-[4vh]">Comprar</button>
             </div>
+            <h2 class="mb-10">À vista no PIX com até 10% OFF</h2>
+            <produtosSimilares />
           </div>
         </div>
 
+        <div class="flex items-center mt-1 bg-white rounded-[20px] p-1 h-[13vh]">
+          <div class="flex items-center text-[2.1vh] ml-5 space-x-4">
+              <img src="../assets/icons/Truck.png" class="h-[6vh]">
+              <h2>Frete e Prazo: </h2>
+              <input type="text" class="flex p-2 justify-center text-center items-center border-[1px] border-black rounded-full placeholder:text-center" placeholder="Digite seu CEP">
+              <button class="bg-[--azul-escuro] w-[20vh] h-[5vh] flex justify-center items-center rounded-3xl text-white">Calcular</button>
+          </div>
+        </div>
       </div>
     </div>
 
-    <div class="flex justify-center items-center ">
-      <produtosSimilares />
-    </div>
 
     <div class="flex justify-center items-center">
       <div class="w-[145vh]">
         <div class="grid grid-cols-[35%_65%] mt-1 justify-items-center space-x-1">
+          
           <fichaTecnica :porte="produto.porte" :idade="produto.idade" :racas="produto.racas"
             :quantidade="produto.quantidade" :sabor="produto.sabor" :pet="produto.categoria" :marca="produto.marca" />
+          
           <descricaoProduto :descricao="produto.descricao" :ingredientes="produto.ingredientes" />
+
         </div>
       </div>
     </div>
