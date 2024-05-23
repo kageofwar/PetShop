41e780679a26219eef4c245d24af46030582113c
Vinicius Casotti de Campos Oliveira <viniciusccoliveira@PREFEITURA.SP.GOV.BR>
2024-05-23 16:29:53 -0300

    start na adm

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import PaginaPricipal from '../views/PaginaPricipal.vue'
 import Produtos from '@/views/Produtos.vue'
 import VisualizarProduto from '@/views/VisualizarProduto.vue'
+import Administracao from '@/views/Administracao.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -20,6 +21,11 @@ const router = createRouter({
       path: '/Produtos/:categoria/:id',
       name: 'VizualizarProduto',
       component: VisualizarProduto
+    },
+    {
+      path: '/administracao',
+      name: 'Administracao',
+      component: Administracao
     }
   ]
 })
