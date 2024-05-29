5a10a970142cfedee7a7d77aec80ab4db2e6bc60
Vinicius Casotti de Campos Oliveira <viniciusccoliveira@PREFEITURA.SP.GOV.BR>
2024-05-29 17:32:54 -0300

    vambora

--- src/views/Administração/PainelProdutos.vue
+++ src/views/Administração/PainelProdutos.vue
@@ -1,5 +1,12 @@
 <template>
-salve
+    <div class="h-[100vh] bg-[--azul-mais-claro]">
+        <div class="grid grid-cols-[10%_25%_40%_25%]">
+            <div></div>
+            <div></div>
+            <div><h1>Gerenciamento de Produtos</h1></div>
+            <div></div>
+        </div>
+    </div>
 </template>
 
 <script>
